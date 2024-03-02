1c3e67e831620877888d136a397aa8eda7ebc1c0
xdaybreakerx <xsalathe@gmail.com>
2024-03-02 13:55:01 +1100

    Add extensions.py, and __init__.py. Complete app config to SQL server hosted on Neon.

---
+++ __init__.py
@@ -0,0 +1,21 @@
+from os import environ
+
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+
+from .extensions import db, ma, bcrypt, jwt
+
+def create_app():
+    app = Flask(__name__)
+
+    # configs
+    app.config["SQLALCHEMY_DATABASE_URI"]=environ.get("DATABASE_URL")
+    app.config["JWT_SECRET_KEY"]=environ.get("JWT_SECRET_KEY")
+
+    # connect libraries with flask app
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    return app
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 # Xander Salathe - Coder Academy - T2A2 - API Webserver Project
+
+## Links:
+- [GitHub](https://github.com/xdaybreakerx/XanderSalathe_T2A2)
+- [Trello](https://trello.com/b/8Lz0gZWa/xander-salathe-t2a2-api-webserver)
+
+## Third Party Tools:
+- PSQL Server hosted via [neon.tech](neon.tech) 
+<!-- @ https://console.neon.tech/app/projects/restless-violet-92821120 -->

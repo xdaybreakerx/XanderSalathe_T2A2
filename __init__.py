dfb5be1158045ecd4c2606916e50f1c32ba7a020
xdaybreakerx <xsalathe@gmail.com>
2024-03-09 14:03:11 +1100

    Add cli_controller.py Add function to create, seed, and drop tables. Seed data added for User model. Blueprint added to init.py

--- __init__.py
+++ __init__.py
@@ -5,17 +5,22 @@ from dotenv import load_dotenv
 
 from .extensions import db, ma, bcrypt, jwt
 
+
 def create_app():
     app = Flask(__name__)
 
     # configs
-    app.config["SQLALCHEMY_DATABASE_URI"]=environ.get("DATABASE_URL")
-    app.config["JWT_SECRET_KEY"]=environ.get("JWT_SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URL")
+    app.config["JWT_SECRET_KEY"] = environ.get("JWT_SECRET_KEY")
 
     # connect libraries with flask app
     db.init_app(app)
     ma.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+
+    from .controllers.cli_controller import db_commands
+
+    app.register_blueprint(db_commands)
 
     return app

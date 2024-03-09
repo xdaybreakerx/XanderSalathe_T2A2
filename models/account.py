700d343aca000f9f9eb96a3bdb03dfb717121aef
xdaybreakerx <xsalathe@gmail.com>
2024-03-09 19:13:19 +1100

    Add Account schema. Add relationship to User Model and schema.

--- models/account.py
+++ models/account.py
@@ -5,20 +5,28 @@ from marshmallow.validate import OneOf
 
 from ..extensions import db, ma
 
-VALID_ROLE = ("User", "Admin", "Auditor")
 
 class Account(db.Model):
-    account_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    __tablename__ = "accounts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("users.id"), nullable=False
+    )  # foreign key
     account_type = db.Column(db.String(50), nullable=False)
     balance = db.Column(db.Numeric(10, 2), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     # Relationship to User
-    user = db.relationship('User', back_populates='accounts')
+    user = db.relationship("User", back_populates="accounts")
+
 
 class AccountSchema(ma.Schema):
-    pass
+    user = fields.Nested("UserSchema", only=["username", "email"])
+
+    class Meta:
+        fields = ("account_id", "user", "account_type", "balance", "date_created")
+
 
 account_schema = AccountSchema()
 accounts_schema = AccountSchema(many=True)

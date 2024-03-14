b9c11aefe55288476a862e876f4927eeffba8e39
xdaybreakerx <xsalathe@gmail.com>
2024-03-14 18:54:38 +1100

    Reorder CRUD operations for Account

--- controllers/account_controller.py
+++ controllers/account_controller.py
@@ -9,6 +9,21 @@ from ..models.account import Account, accounts_schema, account_schema
 accounts_bp = Blueprint("accounts", __name__, url_prefix="/accounts")
 
 
+# http://localhost:8080/accounts - POST
+@accounts_bp.route("/", methods=["POST"])
+@jwt_required()
+def create_account():
+    body_data = request.get_json()
+    account = Account(
+        account_type=body_data.get("account_type"),
+        balance=body_data.get("balance"),
+        user_id=get_jwt_identity(),
+    )
+    db.session.add(account)
+    db.session.commit()
+    return account_schema.dump(account), 201
+
+
 # http://localhost:8080/accounts - GET
 @accounts_bp.route("/")
 def get_all_accounts():
@@ -28,19 +43,19 @@ def get_account(account_id):
         return {"error": f"Account with id {account_id} not found"}, 404
 
 
-# http://localhost:8080/accounts - POST
-@accounts_bp.route("/", methods=["POST"])
-@jwt_required()
-def create_account():
+# http://localhost:8080/accounts/id - PUT, PATCH
+@accounts_bp.route("/<int:account_id>", methods=["PUT", "PATCH"])
+def update_account(account_id):
     body_data = request.get_json()
-    account = Account(
-        account_type=body_data.get("account_type"),
-        balance=body_data.get("balance"),
-        user_id=get_jwt_identity(),
-    )
-    db.session.add(account)
-    db.session.commit()
-    return account_schema.dump(account), 201
+    stmt = db.select(Account).filter_by(id=account_id)
+    account = db.session.scalar(stmt)
+    if account:
+        account.account_type = (body_data.get("account_type") or account.account_type,)
+        account.balance = body_data.get("balance") or account.balance
+        db.session.commit()
+        return account_schema.dump(account)
+    else:
+        return {"error": f"Account with id {account_id} not found"}, 404
 
 
 # http://localhost:8080/accounts/id - DELETE
@@ -52,20 +67,5 @@ def delete_account(account_id):
         db.session.delete(account)
         db.session.commit()
         return {"message": f"Account '{account.account_type}' deleted successfully"}
-    else:
-        return {"error": f"Account with id {account_id} not found"}, 404
-
-
-# http://localhost:8080/accounts/id - PUT, PATCH
-@accounts_bp.route("/<int:account_id>", methods=["PUT", "PATCH"])
-def update_account(account_id):
-    body_data = request.get_json()
-    stmt = db.select(Account).filter_by(id=account_id)
-    account = db.session.scalar(stmt)
-    if account:
-        account.account_type = body_data.get("account_type") or account.account_type,
-        account.balance = (body_data.get("balance") or account.balance)
-        db.session.commit()
-        return account_schema.dump(account)
     else:
         return {"error": f"Account with id {account_id} not found"}, 404

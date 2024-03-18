2f333c458135299393ec1913d295f72ca9f5fe9a
xdaybreakerx <xsalathe@gmail.com>
2024-03-18 21:29:03 +1100

    Created errors directory, and moved error handlers from app.py to handlers.py for separation of concerns

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from os import environ
 
-from flask import Flask, jsonify
-from marshmallow.exceptions import ValidationError
+from flask import Flask
 
 from extensions.extensions import db, ma, bcrypt, jwt
+from errors.handlers import register_error_handlers
 
 
 def create_app():
@@ -20,31 +20,8 @@ def create_app():
     bcrypt.init_app(app)
     jwt.init_app(app)
 
-    # Error handling 
-    @app.errorhandler(400)
-    def bad_request(error):
-        return jsonify({'message': 'Bad Request'}), 400
+    register_error_handlers(app)
 
-    @app.errorhandler(401)
-    def unauthorized(error):
-        return jsonify({'message': 'Unauthorized'}), 401
-
-    @app.errorhandler(404)
-    def not_found(error):
-        return jsonify({'message': 'Not Found'}), 404
-
-    @app.errorhandler(500)
-    def internal_server_error(error):
-        return jsonify({'message': 'Internal Server Error'}), 500
-    
-    @app.errorhandler(405)
-    def method_not_allowed(error):
-        return jsonify({'message': 'Method Not Allowed'}), 405
-    
-    @app.errorhandler(ValidationError)
-    def validation_error(error):
-        return {"error": error.messages}, 400
-    
     from commands.db_commands import db_commands
 
     app.register_blueprint(db_commands)
@@ -52,9 +29,9 @@ def create_app():
     from controllers.auth_controller import auth_bp
 
     app.register_blueprint(auth_bp)
-    
+
     from controllers.account_controller import accounts_bp
-    
+
     app.register_blueprint(accounts_bp)
 
     return app
---
+++ errors/handlers.py
@@ -0,0 +1,27 @@
+from flask import jsonify
+from marshmallow.exceptions import ValidationError
+
+def register_error_handlers(app):
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({'message': 'Bad Request'}), 400
+
+    @app.errorhandler(401)
+    def unauthorized(error):
+        return jsonify({'message': 'Unauthorized'}), 401
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({'message': 'Not Found'}), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({'message': 'Internal Server Error'}), 500
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return jsonify({'message': 'Method Not Allowed'}), 405
+
+    @app.errorhandler(ValidationError)
+    def validation_error(error):
+        return {"error": error.messages}, 400

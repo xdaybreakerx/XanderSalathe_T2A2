c64cf3c63dc5cb585e9d85300123c0ebcda9e2c6
xdaybreakerx <xsalathe@gmail.com>
2024-03-23 23:15:31 +1100

    update comments, and HTTP response status codes for transaction_controller api calls

--- controllers/transaction_controller.py
+++ controllers/transaction_controller.py
@@ -13,42 +13,46 @@ transactions_bp = Blueprint(
 )
 
 
-# Add a new transaction
+# Adds a new transaction to a specified account. This operation checks account ownership and adjusts the account balance.
 # http://localhost:8080/accounts/id/transactions - POST
 @transactions_bp.route("/", methods=["POST"])
 @jwt_required()
 def create_transaction(account_id):
+    # Obtain the current user's ID from the JWT payload
     user_id = get_jwt_identity()
     body_data = request.get_json()
+    # Retrieve the specified account to ensure it exists and determine if the user is authorized to add a transaction
     stmt = db.select(Account).filter_by(id=account_id)
     account = db.session.scalar(stmt)
 
-    # if no account is found by the id, a transaction cannot be added
+    # If no account matches the provided ID, return an error
     if not account:
         return {"error": f"Account with id {account_id} not found"}, 404
 
-    # a user can only add transactions to their own account
-    # a admin can add transactions to any account
-
+    # Allow adding transactions to an account if the user is either an admin or the account owner
     if is_user_in_role(["Admin"]) or int(account.user_id) == int(user_id):
+        # Create a new Transaction object and associate it with the account
         transaction = Transaction(
             amount=body_data.get("amount"),
             description=body_data.get("description"),
             account_id=account_id,
         )
+        # Add the transaction to the session and adjust the account's balance accordingly
         db.session.add(transaction)
 
+        # Update the account balance
         account.balance += body_data.get("amount")
         db.session.add(account)
 
         db.session.commit()
         return transaction_schema.dump(transaction), 201
 
-    # if a user tries to add a transaction to an account they do not own, an authorization error is returned
+    # If the user does not own the account and is not an admin, deny access
     else:
         return {"error": "Unauthorized access"}, 403
 
 
+# Updates an existing transaction, restricted to admin users. This includes adjusting the account balance if the transaction amount changes.
 # http://localhost:8080/accounts/id/transactions/id - PUT, PATCH
 @transactions_bp.route("/<int:transaction_id>", methods=["PUT", "PATCH"])
 @jwt_required()
@@ -56,44 +60,56 @@ def create_transaction(account_id):
 @role_required(["Admin"])
 def update_transaction(account_id, transaction_id):
     body_data = request.get_json()
+    # Retrieve the transaction to be updated, ensuring it exists and is part of the specified account
     stmt = db.select(Transaction).filter_by(id=transaction_id, account_id=account_id)
     transaction = db.session.scalar(stmt)
     if transaction:
+        # Calculate the difference between the new amount and the original amount to adjust the account balance
         old_amount = transaction.amount
         new_amount = body_data.get("amount")
 
+        # Update transaction details
         transaction.amount = body_data.get("amount") or transaction.amount
         transaction.description = (
             body_data.get("description") or transaction.description
         )
 
+        # Adjust the account balance based on the amount difference
         amount_difference = new_amount - old_amount
         transaction.account.balance += amount_difference
 
+        # Commit changes to the database
         db.session.commit()
-        return transaction_schema.dump(transaction)
+        return transaction_schema.dump(transaction), 201
     else:
+        # Return an error if the specified transaction does not exist within the given account
         return {
             "error": f"Transaction with id {transaction_id}, on account {account_id} not found"
         }, 404
 
 
+# Deletes an existing transaction from an account, accessible only to admin users. This also adjusts the account's balance.
 # http://localhost:8080/accounts/id/transactions/id - DELETE
 @transactions_bp.route("/<int:transaction_id>", methods=["DELETE"])
 @jwt_required()
 # Only an Admin can delete a transaction
 @role_required(["Admin"])
 def delete_transaction(account_id, transaction_id):
+    # Retrieve the transaction to ensure it exists within the specified account
     stmt = db.select(Transaction).filter_by(id=transaction_id, account_id=account_id)
     transaction = db.session.scalar(stmt)
     if transaction:
+        # Adjust the account's balance by subtracting the transaction's amount
         transaction.account.balance -= transaction.amount
+        # Remove the transaction from the database
         db.session.delete(transaction)
+        # Commit the deletion to the database
         db.session.commit()
         return {
             "message": f"Transaction with id {transaction_id}, Description: {transaction.description}, Amount: {transaction.amount} deleted successfully"
-        }
+        }, 200
     else:
+        # Return an error if the transaction cannot be found within the specified account
         return {
             "error": f"Transaction with id {transaction_id}, on account {account_id} not found"
         }, 404
